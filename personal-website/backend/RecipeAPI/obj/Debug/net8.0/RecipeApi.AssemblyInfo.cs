e695bb32403e380f8d1b2f66e1134a93bff16337
Maria Şuteu <mara.suteu@gmail.com>
2025-02-04 16:49:54 +0200

    -

--- personal-website/backend/RecipeAPI/obj/Debug/net8.0/RecipeApi.AssemblyInfo.cs
+++ personal-website/backend/RecipeAPI/obj/Debug/net8.0/RecipeApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecipeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5858e3e8e01b5dfd48ed49382d2a223d58fee4a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a88179ee619a4383eb7a2579ffaa92358b4893fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecipeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecipeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
